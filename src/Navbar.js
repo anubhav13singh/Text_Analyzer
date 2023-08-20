4c10c5e5be3dc8447d50ee974cb161b1f819a27e
Anubhav Singh <singhanubhav81717@gmail.com>
2023-08-20 19:21:03 +0530

    all set

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,29 @@
-import { Typography, Stack, Box } from '@mui/material'
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState }from 'react'
+import { NavLink, Link } from 'react-router-dom'
 import './navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
-    const [toggle, setToggle] = useState(false); 
+  const [toggle, setToggle] = useState(false);
   return (
-    <>
-       <Stack direction='row' padding='0px 20px' alignItems='center' justifyContent='space-between' backgroundColor= 'rgb(238,174,202)'>
-         <Typography fontSize={{xs:'6vw', sm:'5vw', md:'3vw'}}>
-            Text Analyzer
-         </Typography>
-         
-         <Box className={ toggle ?' mobile-nav' : 'nav'} >
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/About'>About</NavLink>
-            
-         </Box>
-            
-         <i className='icon' onClick={() => setToggle(!toggle)}> <MenuIcon/> </i>
-       </Stack>
-    </>
+    <nav>
+    <Link to="/" className="title">
+     Text Analyzer
+    </Link>
+    <div className="menu" onClick={() => setToggle(!toggle)}>
+      <MenuIcon/>
+    </div>
+    <ul className={toggle ? "open" : ""}>
+    <li>
+        <NavLink to="/" onClick={() => setToggle(false)}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about" onClick={() => setToggle(false)}>About</NavLink>
+      </li>
+    </ul>
+  </nav>
   )
 }
 
 export default Navbar
+
