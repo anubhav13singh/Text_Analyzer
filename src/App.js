518866b1605ef7dd609181a7dc70bacf55e15728
Anubhav Singh <singhanubhav81717@gmail.com>
2024-01-28 20:13:00 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { useState } from 'react'
-import Navbar from './Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Alert from './Alert'
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Alert from "./Alert";
 
 function App() {
+  const [alert, setAlert] = useState(null);
 
-  const [alert,setAlert] = useState(null);
-
-  const showAlert = (message,type) => {
-      setAlert({
-        msg:message,
-        type:type
-      })
-      setTimeout(() => {
-        setAlert(null)
-      }, 1500);
-  }
-
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   return (
     <>
-    
-    <Navbar />
-    <Alert alert= {alert} />
-    <Routes>
-      <Route path= '/' element={ <Home showAlert= {showAlert}/>}/>
-      <Route path= '/About' element={ <About />}/>
-    </Routes>
-    
+      <Navbar />
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={<Home showAlert={showAlert} />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
